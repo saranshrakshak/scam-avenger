18b24f713903a12744742e84179acf2035f5ef78
saranshrakshak <srakshak@berkeley.edu>
2021-08-23 16:01:47 -0700

    dataframe fixed

--- scam-counter-attack/chatterbox.py
+++ scam-counter-attack/chatterbox.py
@@ -4,37 +4,41 @@
 # pip3 install nltk
 # pip3 install ffmpeg, ffprobe, ffplay from command line
 # and download from https://ffmpeg.org/download.html
-# sentiment models:
+# sentiment models
 # nltk.download("vader_lexicon")
+
 from pydub import AudioSegment
 import speech_recognition as sr
 import os
-import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-#changing format of incoming file from .m4a (or other) to .wav
-#returns a AudioSegment object
+
+# changing format of incoming file from .m4a (or other) to .wav
+# returns a AudioSegment object
 def convert_to_wav(filename):
     filename = 'audio_files/' + filename
+    file_type = filename.split(".")[1]
 
-    if filename.split('.')[1] == '.m4a':
+    if file_type != '.wav':
         new_name = filename.split(".")[0] + ".wav"
         audio = AudioSegment.from_file(filename)
         audio.export(new_name, format='wav')
-        os.remove(filename) #saves space, less search time
         return new_name
     else:
-        if os.path.exists(filename):
-            print('File <<', filename, '>> already converted to .wav and removed.')
-            return filename
+        if os.path.exists(filename.__contains__('.wav')):
+            # print('File <<', filename, '>> already converted to .wav and removed.')
+            return filename.split(".")[0] + ".wav"
         else:
-            print('File <<', filename, '>> non-existent.')
-            return False
+            # print('File <<', filename, '>> non-existent.')
+            return filename.split(".")[0] + ".wav"
+
 
 def get_volume(filename):
+    # print('Volume score: ', AudioSegment.from_file(filename).dBFS)
     return AudioSegment.from_file(filename).dBFS
 
-#converting spoken words to text
+
+# converting spoken words to text
 def transcribe_audio(wav_file):
     if not wav_file: return False
     recognizer = sr.Recognizer()
@@ -45,10 +49,11 @@ def transcribe_audio(wav_file):
     return recognizer.recognize_google(audio_data)
 
 
-#sentiment analysis of speaker's text
+# sentiment analysis of speaker's text
 def analyze_text(text_file):
-    if not text_file: return False
+    if not text_file:
+        return False
     intensity = SentimentIntensityAnalyzer()
-    #print('Conversation Transcription:', text_file)
-    #print('Conversation Sentiment: ', intensity.polarity_scores(text_file))
+    # print('Conversation Transcription:', text_file)
+    # print('Conversation Sentiment: ', intensity.polarity_scores(text_file))
     return intensity.polarity_scores(text_file)
--- scam-counter-attack/main.py
+++ scam-counter-attack/main.py
@@ -1,24 +1,52 @@
+import glob
 from chatterbox import *
 import os
 import numpy as np
 import pandas as pd
 
-full_convo_df = pd.DataFrame()
+call_df = pd.DataFrame(columns = ['scam_audio', 'scam_volume', 'scam_text', 'scam_sentiment'])
+
+
 def file_runner(filename):
+    global call_df
     wav_file = convert_to_wav(filename)
-    volume_file = get_volume(wav_file)
-    text_file = transcribe_audio(wav_file)
-    sentiment_file = analyze_text(text_file)
-    print(wav_file, volume_file, text_file, sentiment_file)
-    return #wav_file, volume_file, text_file, sentiment_file
+
+    if filename.endswith('.m4a'):
+        os.remove('audio_files/' + filename)
+    if call_df['scam_audio'].str.contains(wav_file).any():
+        print('Skip Reprocessing')
+        return
+    else:
+        volume_file = get_volume(wav_file)
+        text_file = transcribe_audio(wav_file)
+        sentiment_file = analyze_text(text_file)
+        dict_row = {'scam_audio': wav_file, 'scam_volume': volume_file, 'scam_text': text_file,
+                    'scam_sentiment': sentiment_file}
+        call_df = call_df.append(dict_row, ignore_index=True)
+        return dict_row
 
 
 def run_all_audio():
     all_files = np.array(os.listdir('audio_files'))
-    print('Files that will be run:', all_files)
+    all_files = all_files[all_files != '.DS_Store']
+
+    print(len(all_files), 'files will be run:', all_files)
+
     for i in all_files:
         if i != '.DS_Store':
+            # print(' ------- RUNNING NEW AUDIO FILE ------- ')
+            # print('File: ' + i)
             file_runner(i)
 
+
+def newest_file():
+    new_file = max(glob.glob('audio_files/*'), key=os.path.getctime)
+    return new_file.split('/')[1]
+
+
+run_all_audio()
+file_runner(newest_file())  # test to avoid duplicate files
+
+#print('CallDF: ', call_df, type(call_df), call_df.columns)
 
 run_all_audio()
